ba8bc54cd8a64e7cb95b103fe6cff0976d07c03e
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-13 23:14:45 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 def meal_choice(side1, side2, protein = "meat")
   order = "A plate of #{protein} with #{side1} and #{side2}."
-  p "What a nutritious meal!"
+  puts "What a nutritious meal!"
   p order
 end

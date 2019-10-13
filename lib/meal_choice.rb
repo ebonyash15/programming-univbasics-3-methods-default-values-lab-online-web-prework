04cd9c57c70957a8f41d02acdf5ce2533439b026
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-13 22:57:29 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,4 @@
 puts "What a nutritious meal!"
 def meal_choice(side1, side2, protein='meat')
-    puts "What a nutritious meal! \n A plate of #{protein} with #{side1} and #{side2}."end
+    puts "What a nutritious meal!"
+    puts "A plate of #{protein} with #{side1} and #{side2}."end
